1e9da06bd2776888d981ec23b3dfead53b190570
1nick19 <nickvl20101@gmail.com>
2024-05-13 11:43:29 +0200

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,18 @@
+basic.forever(function () {
+    basic.showLeds(`
+        . # . # .
+        # # # # #
+        # # # # #
+        . # # # .
+        . . # . .
+        `)
+    basic.clearScreen()
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . # # # .
+        . . # . .
+        . . . . .
+        `)
+    basic.clearScreen()
+})
